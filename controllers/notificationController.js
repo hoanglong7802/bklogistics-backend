533c45eb25c559d742c84d7efc53e3b3b475ac96
hoanglong7802 <hoanglong7802@gmail.com>
2023-06-18 23:28:39 +0700

    Minor fix

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -2,17 +2,12 @@ const express = require('express');
 
 exports.getNotification = async (req, res) => {
     try {
-        const books = [
-            { title: 'Book 1', author: 'Author 1' },
-            { title: 'Book 2', author: 'Author 2' },
-            { title: 'Book 3', author: 'Author 3' },
-          ];
         
-          const booksJson = JSON.stringify(books, null, 2);
+        const message = "OK";
 
-          req.io.emit('bookListUpdated', books);
-          
-          res.json(booksJson);
+        req.io.emit('message', message);
+
+        res.json({message: message});
 
           
     }
