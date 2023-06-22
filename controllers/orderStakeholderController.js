cfb44c82fc6f633388a90d70927604e202af218a
hoanglong7802 <hoanglong7802@gmail.com>
2023-06-22 09:17:51 +0700

    Complete requiredMaterial and orderStakeholder

---
+++ controllers/orderStakeholderController.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const OrderStakeholder = require('../models/orderStakeholderModel');
+
+
+exports.createOrderStakeholder = async (req, res, next) => {
+    try {
+        const {
+            address, 
+            role,
+            supplier_material,
+            manufacturer_product,
+            order_id
+        } = req.body;
+
+        const orderStakeholder = new OrderStakeholder({
+            address, 
+            role,
+            supplier_material,
+            manufacturer_product,
+            order_id
+        });
+
+        const createdOrderStakeholder = await OrderStakeholder.save();
+
+        if (!createdOrderStakeholder) {
+            return res.status(400).json({error: "Unable to create order-stakeholder"})
+        }
+
+        res.json(createdOrderStakeholder);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.getOrderStakeholder = async (req, res, next) => {
+    try {
+        const {
+            address, 
+            role,
+            supplier_material,
+            manufacturer_product,
+            order_id
+        } = req.body;
+
+        const query = {};
+
+        if (address) {
+            query.address = address;
+        }
+
+        if (role) {
+            query.role = role;
+        }
+
+        if (supplier_material) {
+            query.supplier_material = supplier_material;
+        }
+
+        if (manufacturer_product) {
+            query.manufacturer_product = manufacturer_product;
+        }
+
+        const orderStakeholders = await OrderStakeholder.find(query);
+
+        res.json(orderStakeholders);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.getOrderStakeholderById = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        
+        const orderStakeholder = await OrderStakeholder.findById(id);
+
+        res.json(orderStakeholder);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.updateOrderStakeholder = async (req, res, next) => {
+    try {
+
+        const id = req.params.id;
+
+        const {
+            address, 
+            role,
+            supplier_material,
+            manufacturer_product,
+            order_id
+        } = req.body;
+
+        const updatedOrderStakeholder = await OrderStakeholder.findByIdAndUpdate(
+            id,
+            {
+                address, 
+                role,
+                supplier_material,
+                manufacturer_product,
+                order_id
+            },
+            {new: true}
+        );
+
+        if (!updatedOrderStakeholder) {
+            res.status(404).json({error: "Order-Stakeholder not found"});
+        }
+
+        res.json(updatedOrderStakeholder);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.deleteOrderStakeholder = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        
+        const deletedOrderStakeholder = await OrderStakeholder.findByIdAndDelete(id);
+
+        if (!deletedOrderStakeholder) {
+            return res.status(404).json({ error: 'Order not found' });
+          }
+
+        res.json(deletedOrderStakeholder);
+    }
+    catch (error) {
+        next(error);
+    }
+}
---
+++ controllers/requiredMaterialController.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const RequiredMaterial = require('../models/requiredMaterialModel');
+
+
+exports.createRequiredMaterial = async (req, res, next) => {
+    try {
+        const {
+            material_id,
+            product_id,
+            quantity,
+            unit,
+        } = req.body;
+
+        const requiredMaterial = new RequiredMaterial({
+            material_id,
+            product_id,
+            quantity,
+            unit,
+        });
+
+        const createdRequiredMaterial = await RequiredMaterial.save();
+
+        if (!createdRequiredMaterial) {
+            return res.status(400).json({error: "Unable to create required material"})
+        }
+
+        res.json(createdRequiredMaterial);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.getRequiredMaterial = async (req, res, next) => {
+    try {
+        const {
+            material_id,
+            product_id,
+            quantity,
+            unit,
+        } = req.body;
+
+        const query = {};
+
+        if (material_id) {
+            query.material_id = material_id;
+        }
+
+        if (product_id) {
+            query.product_id = product_id;
+        }
+
+        if (unit) {
+            query.unit = unit;
+        }
+
+        const requiredMaterials = await RequiredMaterial.find(query);
+
+        res.json(requiredMaterials);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.getRequiredMaterialById = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        
+        const requiredMaterial = await RequiredMaterial.findById(id);
+
+        res.json(requiredMaterial);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.updateRequiredMaterial = async (req, res, next) => {
+    try {
+
+        const id = req.params.id;
+
+        const {
+            material_id,
+            product_id,
+            quantity,
+            unit,
+        } = req.body;
+
+        const updatedRequiredMaterial = await RequiredMaterial.findByIdAndUpdate(
+            id,
+            {
+                material_id,
+                product_id,
+                quantity,
+                unit,
+            },
+            {new: true}
+        );
+
+        if (!updatedRequiredMaterial) {
+            return res.status(404).json({error: "Order-Stakeholder not found"});
+        }
+
+        res.json(updatedRequiredMaterial);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+exports.deleteRequiredMaterial = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        
+        const deletedRequiredMaterial = await RequiredMaterial.findByIdAndDelete(id);
+
+        if (!deletedRequiredMaterial) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+
+        res.json(deletedRequiredMaterial);
+    }
+    catch (error) {
+        next(error);
+    }
+}
